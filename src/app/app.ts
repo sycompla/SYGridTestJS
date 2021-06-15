78725230bc2df1233450514abc150ae1e3c0d93b
d7p4n4 <Szappan60>
2021-06-15 16:12:34 +0200

    1.1.20210615.3.bzs

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,13 +5,16 @@ Grid.Inject(Page, Sort, Filter, Group);
 let grid: Grid = new Grid({
     dataSource: data,
     columns: [
-                { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120, type: 'number' },
-                { field: 'CustomerID', width: 140, headerText: 'Customer ID', type: 'string' },
-                { field: 'Freight', headerText: 'Freight', textAlign: 'Right', width: 120, format: 'C' },
-                { field: 'OrderDate', headerText: 'Order Date', width: 140, format: 'yMd' }
+                { field: 'OrderID', headerText: 'Order ID', textAlign: 'Center', width: 120, type: 'number', headerTextAlign: 'Center' },
+                { field: 'CustomerID', width: 140, headerText: 'Customer ID', type: 'string', textAlign: 'Center', headerTextAlign: 'Center' },
+                { field: 'Freight', headerText: 'Freight', textAlign: 'Center', width: 120, format: 'C', headerTextAlign: 'Center' },
+                { field: 'OrderDate', headerText: 'Order Date', width: 140, format: 'yMd', textAlign: 'Center', headerTextAlign: 'Center' }
     ],
     allowGrouping: true,
     allowFiltering: true,
+    filterSettings: {
+        mode: 'Immediate'
+    },
     allowSorting: true,
     allowPaging: true,
     pageSettings: { pageSize: 7 }
